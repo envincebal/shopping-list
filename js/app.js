d1f5d807dee3371b3b2018f28cbc4c3bef52de4b
Vincent Yan <vincentyan1@gmail.com>
2015-12-21 18:22:43 -0800

    Update app.js code

--- js/app.js
+++ js/app.js
@@ -1,34 +1,23 @@
 $(document).ready(function(){
-
-	/* Adds item into list by pressing "Enter" */
-	$("#entry").keypress(function(event){
-		if (event.which == 13){
-		$('ul').prepend('<li><input type="checkbox" class="checkbox">' + $("#entry").val() + '<div class="minus"><i class="fa fa-minus"></i></div></li>');
-			$('#entry').val("");
-		}/*else{	
-		$(("#entry").val().length == 0){
-			alert("Type something in the field");
-		};*/
-	});
-
-	/* Adds item into list by clicking "add item" button */
-	$("#add-item").click(function(){
+	/* Adds item by pressing "Enter" or clicking "Add Itme" */
+	$("#entryForm").submit(function(event){
 		if ($("#entry").val().length == 0){
-			alert("Type something in the field");
+			$(".alert").html("Type something in the field");
 		}else{
 			$('ul').prepend('<li><input type="checkbox" class="checkbox">' + $("#entry").val() + '<div class="minus"><i class="fa fa-minus"></i></div></li>');
 			$('#entry').val("");
 		};
+		return false;
 	});
-
+	
 	/* Crosses off item */
 	$("ul").on("click", ".checkbox", function(){
-		$(this).closest("li").toggleClass("strike");
+		$(this).parent().toggleClass("strike");
 	});
 
 	/* Deletes item */
 	$("ul").on("click", ".minus", function(){
-		$(this).closest("li").remove();
+		$(this).parent().remove();
 	});
 
 	/* Deletes all checked items */

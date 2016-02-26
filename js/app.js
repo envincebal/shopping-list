e5c1cb899fc11f2f4e5dbaa3d58bf9d34a6899f0
Vincent Yan <vincentyan1@gmail.com>
2016-02-25 23:23:03 -0800

    Change color scheme and rearrange elements

--- js/app.js
+++ js/app.js
@@ -5,11 +5,7 @@ $(document).ready(function(){
 			$(".alert").html("Type something in the field").show();
 		}else{ 
 			var userInput = $("#entry").val();
-<<<<<<< HEAD
-			$("ul").prepend("<li><input type=\"checkbox\" class=\"checkbox\">" + userInput + "<div class=\"minus\"><i class=\"fa fa-minus\"></i></div></li>");
-=======
 			$("ul").append("<li><input type=\"checkbox\" class=\"checkbox\">" + userInput + "<div class=\"minus\"><i class=\"icon-remove\"></i></div></li>");
->>>>>>> master
 			$("#entry").val("");
 			$(".alert").hide();
 		};
@@ -27,15 +23,6 @@ $(document).ready(function(){
 	});
 
 	/* Deletes item */
-<<<<<<< HEAD
-	$("ul").on("click", ".minus", function(event){
-		$(this).parent().remove();
-	});
-
-	/* Deletes all checked items */
-	$("#clear-checked").on("click", function(){
-		$(".strike").remove();
-=======
 	$("ul").on("click", ".minus", function(){
 		$(this).parent().fadeOut();
 	});
@@ -43,7 +30,6 @@ $(document).ready(function(){
 	/* Deletes all checked items */
 	$("#clear-checked").click(function(){
 		$(".strike").fadeOut();
->>>>>>> master
 	});
 
 	/* Allows for rearranging list items by clicking and dragging */
